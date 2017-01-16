6cc36302cc71de1ce54eef0086e15fba9be2fa52
Guillaume Marty <edo999@gmail.com>
2017-01-16 15:51:51 +0000

    Add a map view

--- js/scenes/Map.js
+++ js/scenes/Map.js
@@ -3,8 +3,10 @@ import {
   View,
   Text,
   TouchableOpacity,
+  Dimensions,
   StyleSheet,
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 import { defaultTextStyle } from '../../config';
 
@@ -21,8 +23,22 @@ export default class Map extends Component {
   }
 
   render() {
+    const height = Dimensions.get('window').height;
+    const width = Dimensions.get('window').width;
+
     return (
       <View>
+        <View style={[styles.container, { height, width }]}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 51.525,
+              longitude: -0.078315,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
         <Text style={styles.header}>Map</Text>
         <TouchableOpacity
           onPress={this.onBackPress}>
@@ -43,8 +59,17 @@ const styles = StyleSheet.create({
     fontSize: 22,
     textAlign: 'center',
   },
-
   text: {
     ...defaultTextStyle,
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 300,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
