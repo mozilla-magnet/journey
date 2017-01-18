cdcd812286668c04e44b02a898b1dd6b5c2b8251
Guillaume Marty <edo999@gmail.com>
2017-01-18 13:33:58 +0000

    Wire scenes with Redux placeholders

--- __tests__/js/App.js
+++ __tests__/js/App.js
@@ -1,11 +1,16 @@
 import 'react-native';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from '../../js/App';
 
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
+  const store = createStore(() => {});
   const tree = renderer.create(
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   );
 });
