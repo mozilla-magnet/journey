23a8d296ff89c2c13c031fe73bfebf6cb138ab27
Guillaume Marty <edo999@gmail.com>
2017-01-18 13:08:37 +0000

    Simplify the structure of the app

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,4 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StatusBar,
-  View,
-} from 'react-native';
-import { theme } from './config';
+import { AppRegistry } from 'react-native';
 import AppProvider from './js/AppProvider';
 
-export default class MagnetJourney extends Component {
-  render() {
-    return (
-      <View style={{
-        backgroundColor: theme.colorBackground,
-        flex: 1,
-      }}>
-        <StatusBar
-          translucent={true}
-          backgroundColor="rgba(255, 255, 255, 0.7)"
-          barStyle="light-content"/>
-        <AppProvider/>
-      </View>
-    );
-  }
-}
-
-AppRegistry.registerComponent('MagnetJourney', () => MagnetJourney);
+AppRegistry.registerComponent('MagnetJourney', () => AppProvider);
